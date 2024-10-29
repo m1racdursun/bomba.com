a45abc07a05aa17f97472ba179c6f3cdbf3baca6
m1racdursun <dursunmirac2@gmail.com>
2024-10-29 21:01:04 +0300

    index dosyası düzenlendi

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Proje 1</title>
+    <title>BOMBALIK</title>
 </head>
 <body>
     <h1>Proje-1 Admin Sayfası</h1>
