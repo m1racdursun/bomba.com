c2598ea90177237387e62b6216a338ba9f39e87a
m1racdursun <dursunmirac2@gmail.com>
2024-10-29 20:58:50 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Proje 1</title>
+</head>
+<body>
+    <h1>Proje-1 Admin Sayfası</h1>
+    <h2>Bu projede emeği geçenler</h2>
+    <ul>
+        <li>Mehmet Mirac DURSUN</li>
+        <li>Enes Malik GABLAN</li>
+        <li>Emre ŞİŞMAN</li>
+        <li>Eda POLAT</li>
+    </ul>
+</body>
+</html>
